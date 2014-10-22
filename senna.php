cba81daa84fa494df8c775ab2fd52b7104dda1a8
Fernando Aureliano <mail@fernandoaureliano.com>
2014-10-21 22:17:27 -0300

    first commit

--- senna.php
+++ senna.php
@@ -12,19 +12,20 @@
  * Version:           0.0.1
  * Author:            Fernando Aureliano
  * Author URI:        http://fernandoaureliano.com
- * Text Domain:       wp-conversion-boxes
+ * Text Domain:       
  * Domain Path:       /lang/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
+function senna() {
+	wp_enqueue_script(
+		'custom-script',
+		get_stylesheet_directory_uri() . '/js/senna.js',
+	);
+}
 
-
-
-
-
-
-
+add_action( 'wp_enqueue_scripts', 'senna' );
 
 
 ?>
