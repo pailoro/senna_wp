b385072d248ccff4f059c27e9a7ca9afdc11fbb8
Fernando Aureliano <mail@fernandoaureliano.com>
2014-10-21 21:58:46 -0300

    start plugin

---
+++ senna.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package   Sena_WP
+ * @author    Fernando Aureliano <mail@fernandoaureliano.com>
+ * @license   GPL-2.0+
+ * @copyright 2014 Fernando Aureliano
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Senna WP
+ * Plugin URI:        http://fernandoaureliano.com
+ * Description:       This plugin implements sennajs in your wordpress theme.
+ * Version:           0.0.1
+ * Author:            Fernando Aureliano
+ * Author URI:        http://fernandoaureliano.com
+ * Text Domain:       wp-conversion-boxes
+ * Domain Path:       /lang/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+
+
+
+
+
+
+
+
+
+?>
