9afdee9ac8169c9aa6d0ae719e522b194b653b75
Fernando Aureliano <mail@fernandoaureliano.com>
2014-10-24 15:45:37 -0300

    adding comments on senna_init.js

--- init_senna.js
+++ init_senna.js
@@ -1,3 +1,4 @@
+// Creating senna data attributes to load senna
 document.getElementsByTagName("body")[0].setAttribute("data-senna","");
 document.getElementById("content").setAttribute("data-senna-surface","");
 
